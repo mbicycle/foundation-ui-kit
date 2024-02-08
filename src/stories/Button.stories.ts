a21b083acedf6c642fd474cbc34a03a85b3ee349
HlebPinchukou <hleb.pinchukou@mbicycle.com>
2024-02-08 12:09:08 +0300

    Add Search

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,4 +1,4 @@
-import { Button } from '../components/button';
+import { Button } from '../components/Button';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta = {
--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -1,4 +1,4 @@
-import { Card } from '../components/card';
+import { Card } from '../components/Card';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta = {
