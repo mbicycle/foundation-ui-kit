6c7de04f21addfead153bf46525d1e40d41e6e6e
HlebPinchukou <hleb.pinchukou@mbicycle.com>
2024-02-14 13:39:46 +0300

    Add ProgressBar

---
+++ src/components/ProgressBar/index.tsx
@@ -0,0 +1,33 @@
+type ProgressBarProps = {
+    progress: number;
+    size?: 'small' | 'default' | 'large' | 'extra-large';
+};
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ progress, size = 'default' }) => {
+    let barHeight = '';
+    switch (size) {
+        case 'small':
+            barHeight = 'h-1.5';
+            break;
+        case 'large':
+            barHeight = 'h-4';
+            break;
+        case 'extra-large':
+            barHeight = 'h-6';
+            break;
+        default:
+            barHeight = 'h-2.5';
+            break;
+    }
+
+    return (
+        <div className={`w-full bg-gray-200 rounded-full ${barHeight} dark:bg-gray-700`}>
+            <div
+                className={`bg-blue-600 rounded-full dark:bg-blue-500 ${barHeight}`}
+                style={{ width: `${progress}%` }}
+            ></div>
+        </div>
+    );
+};
+
+export default ProgressBar;
---
+++ src/stories/ProgressBar.stories.ts
@@ -0,0 +1,48 @@
+import ProgressBar from '../components/ProgressBar';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta = {
+    title: 'Example/ProgressBar',
+    component: ProgressBar,
+    argTypes: {
+        progress: {
+            control: { type: 'range', min: 0, max: 100, step: 1 },
+        },
+        size: {
+            control: { type: 'select', options: ['small', 'default', 'large', 'extra-large'] },
+        },
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        progress: 45,
+    },
+};
+
+export const Small: Story = {
+    args: {
+        progress: 25,
+        size: 'small',
+    },
+};
+
+export const Large: Story = {
+    args: {
+        progress: 70,
+        size: 'large',
+    },
+};
+
+export const ExtraLarge: Story = {
+    args: {
+        progress: 90,
+        size: 'extra-large',
+    },
+};
+
