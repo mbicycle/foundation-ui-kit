b426cd1014fd4c76ff9c6b264b6260c7c3e3168c
HlebPinchukou <hleb.pinchukou@mbicycle.com>
2024-02-14 16:42:21 +0300

    Add Spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,35 @@
+import Icon from './icon';
+
+type SpinnerProps = {
+    className?: string;
+    size?: 'small' | 'medium' | 'large' | 'extra-large';
+};
+
+const Spinner: React.FC<SpinnerProps> = ({ className = '', size = 'medium' }) => {
+    let iconSize = '';
+    switch (size) {
+        case 'small':
+            iconSize = 'w-4 h-4';
+            break;
+        case 'medium':
+            iconSize = 'w-6 h-6';
+            break;
+        case 'large':
+            iconSize = 'w-8 h-8';
+            break;
+        case 'extra-large':
+            iconSize = 'w-10 h-10';
+            break;
+        default:
+            break;
+    }
+
+    return (
+        <div role="status" className={className}>
+            <Icon className={`inline animate-spin text-gray-200 dark:text-gray-600 fill-blue-600 ${iconSize}`} />
+            <span className="sr-only">Loading...</span>
+        </div>
+    );
+};
+
+export default Spinner;
---
+++ src/stories/Spinner.stories.ts
@@ -0,0 +1,40 @@
+import Spinner from '../components/Spinner';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta = {
+  title: 'Example/Spinner',
+  component: Spinner,
+  argTypes: {
+    size: {
+      options: ['small', 'large', 'extra-large'],
+      control: { type: 'select' },
+    },
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const SmallSpinner: Story = {
+  args: {
+    size: 'small',
+  },
+};
+
+export const LargeSpinner: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const ExtraLargeSpinner: Story = {
+  args: {
+    size: 'extra-large',
+  },
+};
