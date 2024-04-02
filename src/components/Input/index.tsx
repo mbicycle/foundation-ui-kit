3808cdc0be7de9a0fc093b3be8742ddd0bcd2b5d
HlebPinchukou <hleb.pinchukou@mbicycle.com>
2024-04-02 13:41:52 +0300

    Fixes

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,7 +5,8 @@ type InputProps = {
     id: string;
     label: string;
     placeholder: string;
-    className?: string;
+    inputClasses?: string;
+    wrapperClasses?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 export function Input({
@@ -13,18 +14,19 @@ export function Input({
                           id,
                           label,
                           placeholder,
-                          className = '',
+                          inputClasses = '',
+                          wrapperClasses = '',
                           ...rest
                       }: InputProps) {
     return (
-        <div className={`mb-6 ${className}`}>
-            <label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <div className={`mb-6 ${wrapperClasses}`}>
+            <label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900">
                 {label}
             </label>
             <input
                 type={type}
                 id={id}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${inputClasses}`}
                 placeholder={placeholder}
                 required
                 {...rest}
