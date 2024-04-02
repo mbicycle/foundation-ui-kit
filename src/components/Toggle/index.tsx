1884d24cc20de7b539fbb7a64cf53338eb2f9b28
TaniaPashkevich <pashkevich.tatsiana@mbicycle.com>
2024-04-02 11:21:26 +0300

    fix toggle

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,18 +1,12 @@
-import {InputHTMLAttributes, useState} from 'react';
+import {InputHTMLAttributes} from 'react';
 
 type ToggleProps = {
-    checked?: boolean;
     text?: string;
     color?: 'blue' | 'red' | 'yellow';
     className?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-function Toggle({ checked, text, color, className }: ToggleProps) {
-    const [isChecked, setIsChecked] = useState(checked);
-
-    const handleToggle = () => {
-        setIsChecked(!isChecked);
-    };
+function Toggle({  text, color, className, ...rest }: ToggleProps) {
 
     let toggleColor: string;
     switch (color) {
@@ -38,8 +32,7 @@ function Toggle({ checked, text, color, className }: ToggleProps) {
                 type="checkbox"
                 value=""
                 className="sr-only peer"
-                checked={isChecked}
-                onChange={handleToggle}
+                {...rest}
             />
             <div
                 className={`w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:${toggleColor}`}
