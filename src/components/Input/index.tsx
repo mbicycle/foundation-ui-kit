7bc41c749f8d35bdefcd72861f838b0994634a89
Tania <pashkevichtania2004@gmail.com>
2024-04-03 14:21:23 +0300

    fix linter

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes } from 'react';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement > {
     type?: 'text' | 'tel' | 'url' | 'email' | 'password' | 'number';
     id?: string;
     label?: string;
