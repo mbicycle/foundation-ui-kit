6233b1f362716e2e3cfd9a12ecaac563373bfd92
Pashkevich Tania <pashkevich.tatsiana@mbicycle.com>
2024-04-17 10:23:29 +0300

    add datepicker

---
+++ src/components/Datepicker/index.tsx
@@ -0,0 +1,72 @@
+import { ForwardedRef, forwardRef } from 'react';
+import DatePicker, {ReactDatePickerProps} from "react-datepicker";
+import {Input, InputProps} from "components/Input";
+import { CalendarDaysIcon } from "@heroicons/react/24/solid"
+import * as React from "react";
+
+
+export interface DatepickerProps extends ReactDatePickerProps {
+  label?: string;
+  inputClasses?: string;
+  wrapperClasses?: string;
+  helperText?: string;
+  helperTextClassName?: string;
+  error?: boolean;
+  inputProps?: InputProps
+}
+
+export const Datepicker = forwardRef(({
+                                   selected,
+                                   onChange,
+                                   label,
+                                   inputClasses = '',
+                                   wrapperClasses = '',
+                                   helperText,
+                                   helperTextClassName = '',
+                                   error,
+                                   inputProps,
+                                   ...rest
+                                 }: DatepickerProps,
+                                 ref: ForwardedRef<HTMLInputElement | HTMLTextAreaElement>
+) => {
+
+    const renderMonthContent = (_: number, shortMonthText: string, fullMonthText: string): React.ReactNode => {
+        const tooltipText = fullMonthText;
+        return <span title={tooltipText} className="p-2">{shortMonthText}</span>;
+    };
+
+  return (
+      <div className={`${wrapperClasses}`}>
+        {label && (
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              {label}
+            </label>
+        )}
+        <DatePicker
+            selected={selected}
+            onChange={onChange}
+            customInput={<Input
+                value={selected?.toString()}
+                type="text"
+                inputClasses={inputClasses}
+                placeholder="Enter Date"
+                icon={<CalendarDaysIcon className="size-6" />}
+                ref={ref}
+                error={error}
+                {...inputProps}
+            />}
+            renderMonthContent={renderMonthContent}
+            closeOnScroll={true}
+            {...rest}
+        />
+        {helperText && (
+            <span className={`block mt-1 text-sm text-gray-500 
+                ${error ? "text-red-500" : "text-gray-500"}
+                 ${helperTextClassName}`}
+            >
+                  {helperText}
+                </span>
+        )}
+      </div>
+  );
+})
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,6 +2,7 @@ import { ForwardedRef, forwardRef, InputHTMLAttributes, RefObject } from 'react'
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement > {
     type?: 'text' | 'tel' | 'url' | 'email' | 'password' | 'number';
+    icon?: React.ReactNode;
     label?: string;
     inputClasses?: string;
     wrapperClasses?: string;
@@ -15,6 +16,7 @@ export const Input = forwardRef(({
                           type,
                           id,
                           label,
+                          icon,
                           inputClasses = '',
                           wrapperClasses = '',
                           multiline = false,
@@ -26,26 +28,31 @@ export const Input = forwardRef(({
     const InputComponent = multiline ? 'textarea' : 'input';
 
     return (
-        <div className={`${wrapperClasses}`}>
+        <div className={`relative  ${wrapperClasses}`}>
             {label && (
                 <label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900">
                     {label}
                 </label>
             )}
+            {icon &&
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                    {icon}
+                </div>}
             <InputComponent
                 type={type}
+                {...rest}
                 className={`${error ? 'border-red-500' : 'border-gray-300'} 
                 bg-transparent border text-gray-900 rounded-lg block w-full p-2.5 resize-none
                 focus:ring-blue-500 focus:border-blue-500
-                disabled:bg-gray-100 autofill:bg-transparent
-                ${multiline ? "min-h-28" : ""}
+                disabled:bg-gray-100 autofill:bg-transparent 
+                ${multiline ? " min-h-28" : ""}
+                ${icon ? " ps-12" : ""}
                 ${inputClasses}`}
-                {...rest}
                 ref={ref as RefObject<HTMLTextAreaElement> & RefObject<HTMLInputElement>}
             />
             {helperText && (
-                <span className={`block mt-1 text-sm text-gray-500 
-                ${error ? "text-red-500" : "text-gray-500"}
+                <span className={`absolute block mt-1 text-sm text-gray-500 
+                ${error ? " text-red-500" : " text-gray-500"}
                  ${helperTextClassName}`}
                 >
                   {helperText}
